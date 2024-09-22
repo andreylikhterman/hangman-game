f0fca27a5ac1fdf2249198904143b1f87f036404
Андрей Лихтерман <andreylikhterman@gmail.com>
2024-09-22 23:58:32 +0300

    Delete the field 'Windows'

--- internal/domain/player.go
+++ internal/domain/player.go
@@ -6,23 +6,14 @@ import (
 
 type Player struct {
 	CountAttempts int
-	Window        *Windows
 }
 
-func (player *Player) ChooseLevel() (level int) {
-	if _, err := fmt.Scan(&level); err != nil {
-		player.Window.SelectLevel()
-		return player.ChooseLevel()
-	}
-
-	return level
+func (player *Player) ChooseLevel() (level int, err error) {
+	_, err = fmt.Scan(&level)
+	return level, err
 }
 
-func (player *Player) ChooseCategory() (category int) {
-	if _, err := fmt.Scan(&category); err != nil {
-		player.Window.SelectCategory()
-		return player.ChooseCategory()
-	}
-
-	return category
+func (player *Player) ChooseCategory() (category int, err error) {
+	_, err = fmt.Scan(&category)
+	return category, err
 }
